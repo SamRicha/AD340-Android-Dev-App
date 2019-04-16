d332f0e617e42cfd6e2b29d38c20108d3b56c515
SamRicha <toolbax@gmail.com>
2019-04-16 12:11:52 -0700

    Add UI buttons with each one having a unique toast

--- app/src/main/java/com/example/appleo/MainActivity.java
+++ app/src/main/java/com/example/appleo/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.appleo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void displayToast(View v) {
+
+        Toast.makeText(MainActivity.this,"Message Sent",Toast.LENGTH_SHORT).show();
+    }
+
+    public void displayToast1(View v) {
+        Toast.makeText(MainActivity.this,"Carrots",Toast.LENGTH_SHORT).show();
+    }
+
+    public void displayToast2(View v) {
+        Toast.makeText(MainActivity.this,"Jelly",Toast.LENGTH_SHORT).show();
+    }
+
+    public void displayToast3(View v) {
+        Toast.makeText(MainActivity.this,"Cookies",Toast.LENGTH_SHORT).show();
+    }
+
+    public void displayToast4(View v) {
+        Toast.makeText(MainActivity.this,"Eggs",Toast.LENGTH_SHORT).show();
     }
 }
